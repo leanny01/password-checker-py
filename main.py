0ec5c92152788ee6203a91ddc8163e2ffd141c8f
leanny01 <dleannykalangu@gmail.com>
2021-05-03 19:27:14 +0200

    done setting initail answers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import passwordChecker
 
-name = input("Please input your name: ")
-surname = input("Please input your surname: ")
-password = input("Please input your password: ")
-passConf = input("Please confirm your password: ")
-passwordChecker.register(name, surname, password, passConf)
+# name = input("Please input your name: ")
+# surname = input("Please input your surname: ")
+# password = input("Please input your password: ")
+# passConf = input("Please confirm your password: ")
+passwordChecker.register()
--- passwordChecker.py
+++ passwordChecker.py
@@ -1,14 +1,50 @@
 def checkPassword(password, passwordConf):
-    # Your code here
-    return
+    has_a_number = False
+    has_uppercase = False
+    # check if both passowrd do not match
+    if password != passwordConf:
+        return False
+    # checking the pasword length
+
+    if len(passwordConf) < 8:
+        return False
+    # check for a number in the password
+
+    for el in password:
+        if el.isnumeric():
+            has_a_number = True
+    if has_a_number == False:
+        return False
+    # check for uppercase
+    for el in password:
+        if el.isupper():
+            has_uppercase = True
+    if has_uppercase == False:
+        return False
+
+    # return True if all is well
+
+    return True
 
 
 def displayDetails(name, surname, password, passConf):
-    # Your code here
-    return
+
+    print(
+        f"\n\nHi {name},\n\nyour full name is : {name +' '+surname}.\nyour password {'*'*len(password) }\n\nBest\nAlliance Team. ")
 
 
-def register(name, surname, password, passConf):
-    checkPassword(password, passConf)
+def register():
+
+    name = input("Please input your name: ")
+    surname = input("Please input your surname: ")
+
+    while True:
+        password = input("Please input your password: ")
+        passConf = input("Please confirm your password: ")
+        if checkPassword(password, passConf) == False:
+            print(f"\n\nPassword is incorrect!!! Please make sure the followings are correct:\n\n1.\tPassowrd length is atleast 8 or more characters\n2.\tpassword has atleast one numeric\n3.\tpassword has atleast one uppercase\n4.\tpassword and password confirmation should match\n\n")
+        else:
+            break
+
     displayDetails(name, surname, password, passConf)
     return
